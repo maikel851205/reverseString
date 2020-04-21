fa405a2f27b6a51ec41854f239865a7ad9bb296c
Maikel <maikel.sanchezguti@gmail.com>
2020-04-21 13:05:46 -0400

    Add project files.

---
+++ reverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main()
+        {
+            static void ReverseString(string str)
+            {
+                string reverse = "";
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    reverse += str[i];
+                }
+
+                Console.WriteLine(reverse);
+            }
+
+
+            string str = "123456";
+
+            ReverseString(str);
+            Console.ReadLine();
+        }
+    }
+}
